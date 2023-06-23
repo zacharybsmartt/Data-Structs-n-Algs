157f9bbc55dd4a81e50d485057ed36042701b8e3
Zachary Bucknor-Smartt <zacharybsmartt@gmail.com>
2023-06-23 15:21:29 -0500

    First part of LinkedList class

--- linkedlist.py
+++ linkedlist.py
@@ -3,4 +3,10 @@ print("test")
 class Node:
     def __init__(self, data = None):
         self.data = data
-        self.next = None
+        self.next_node = None
+
+class LinkedList:
+    def __init__(self):
+        # we create the head node, this is the crux of linked list
+        self.head = None
+        
