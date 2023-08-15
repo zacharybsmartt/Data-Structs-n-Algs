e02e70af8475f0b3cd862e2948943ef4570b8f55
Zachary Bucknor-Smartt <zacharybsmartt@gmail.com>
2023-08-15 07:46:36 -0500

    Starting again

--- linkedlist.py
+++ linkedlist.py
@@ -1,11 +1,1 @@
-print("test")
-
-class Node:
-    def __init__(self, data = None):
-        self.data = data
-        self.next_node = None
-
-class LinkedList:
-    def __init__(self):
-        # we create the head node, this is the crux of linked list
-        self.head = None
+print("starting again")
